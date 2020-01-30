f568c653af533d8d6fd6880769cee4f03888d633
clap01 <cranberry_lab@hotmail.co.jp>
2020-01-30 23:45:55 +0900

    第一フェーズ

--- app.js
+++ app.js
@@ -6,10 +6,24 @@ const puppeteer = require('puppeteer');
 //    slowMo:100
   });
   const page = await browser.newPage();
+  page.setViewport({width: 1200, height: 1200})
   await page.goto('https://kakaku.com/');
-  await page.type('input[id=query]', "ニコン");
+  await page.type('input[id=query]', "PS2" );
   await page.click('input[id=main_search_button]');
+//  await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
+  await page.waitForSelector(".p-item_visual_entity")
+  await page.waitFor(1000)  
+  await page.click('.p-item_visual_entity');
+  console.log('hey1')
+  
+  await page.waitForSelector(".storeName")
+  await page.waitFor(1000)  
+  await page.click('.storeName');
+  console.log('hey2')
+
 
   //await page.screenshot({path: 'screenshot.png'});
   //await browser.close();
 })();
+
+//c-positioning_cell
