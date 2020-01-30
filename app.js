5942681867006b80c13ae8b72d9118dd41465889
clap01 <cranberry_lab@hotmail.co.jp>
2020-01-30 22:13:14 +0900

    基本構文

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: false,
+//    slowMo:100
+  });
   const page = await browser.newPage();
-  await page.goto('https://www.google.com');
-  await page.screenshot({path: 'screenshot.png'});
+  await page.goto('https://kakaku.com/');
+  await page.type('input[id=query]', "ニコン");
+  await page.click('input[id=main_search_button]');
 
-  await browser.close();
+  //await page.screenshot({path: 'screenshot.png'});
+  //await browser.close();
 })();
