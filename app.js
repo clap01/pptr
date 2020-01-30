a09a1a35ddf080352b900bca707d4aa37f7a3885
clap01 <cranberry_lab@hotmail.co.jp>
2020-01-31 00:25:29 +0900

    定数化

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const keyword = "ps4 本体";
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -8,19 +9,17 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
   page.setViewport({width: 1200, height: 1200})
   await page.goto('https://kakaku.com/');
-  await page.type('input[id=query]', "PS2" );
+  await page.type('input[id=query]', keyword);
   await page.click('input[id=main_search_button]');
 //  await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
   await page.waitForSelector(".p-item_visual_entity")
-  await page.waitFor(1000)  
+//  await page.waitFor(1000)  
   await page.click('.p-item_visual_entity');
-  console.log('hey1')
-  
+  console.log('first click!')
   await page.waitForSelector(".storeName")
-  await page.waitFor(1000)  
+//  await page.waitFor(1000)  
   await page.click('.storeName');
-  console.log('hey2')
-
+  console.log('second click!')
 
   //await page.screenshot({path: 'screenshot.png'});
   //await browser.close();
